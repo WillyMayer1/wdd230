cfc95c83fcb5de1d3d0812219cb11dc812d2329c
WillyMayer1 <willianmayercl@gmail.com>
2023-01-31 21:44:18 -0300

    checkpoint

--- week05/scripts/prophets.js
+++ week05/scripts/prophets.js
@@ -15,21 +15,27 @@ function displayProphets(prophets) {
     prophets.forEach((prophet) => {
         // Create elements to add to the div.cards element
         let card = document.createElement('section');
-        let fullName = document.createElement('h2'); // fill in the blank
+        let fullName = document.createElement('h2');
         let portrait = document.createElement('img');
+        let infoDate = document.createElement('p');
+        let infoPlace = document.createElement('p');
 
         // Build the h2 content out to show the prophet's full name
         fullName.textContent = `${prophet.name} ${prophet.lastname}`; // fill in the blank
 
         // Build the image portrait by setting all the relevant attributes
         portrait.setAttribute('src', prophet.imageurl);
-        portrait.setAttribute('alt', `Portait of ${prophet.name} ${prophet.lastname}`); // fill in the blank
+        portrait.setAttribute('alt', `Portait of ${prophet.name} ${prophet.lastname}`);
+        infoDate.innerHTML = `<span class="label">Birth:</span> ${prophet.birthdate}`;
+        infoPlace.innerHTML = `<span class="label">Place:</span> ${prophet.birthplace}`;
         portrait.setAttribute('loading', 'lazy');
         portrait.setAttribute('width', '340');
         portrait.setAttribute('height', '440');
 
         // Append the section(card) with the created elements
         card.appendChild(fullName); //fill in the blank
+        card.appendChild(infoDate);
+        card.appendChild(infoPlace);
         card.appendChild(portrait);
 
         cards.appendChild(card);
