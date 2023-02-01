669a2a49db227173e95b3b6b71a559537ab9dd78
WillyMayer1 <willianmayercl@gmail.com>
2023-02-01 10:49:33 -0300

    checkpoint

--- week05/scripts/weather.js
+++ week05/scripts/weather.js
@@ -4,15 +4,15 @@ const currentTemp = document.querySelector('#current-temp');
   const weatherIcon = document.querySelector('#weather-icon');
   const captionDesc = document.querySelector('figcaption');
 
-  const url = 'https://api.openweathermap.org/data/2.5/weather?q=Trier&appid=24a1aa44fb130edfe986a180a96be51a';
+  const url = 'https://api.openweathermap.org/data/2.5/weather?q=Trier,units=imperial&appid=24a1aa44fb130edfe986a180a96be51a';
 
   async function apiFetch() {
     try {
       const response = await fetch(url);
       if (response.ok) {
         const data = await response.json();
-        console.log(data); // testing only
-        // displayData(data); // uncomment when ready
+        console.log(data);
+        displayData(data);
       } else {
           throw Error(await response.text());
       }
@@ -22,3 +22,14 @@ const currentTemp = document.querySelector('#current-temp');
   }
   
   apiFetch();
+
+  function displayResults(data) {
+    currentTemp.innerHTML = `${data.main.temp}&deg;F`;
+    const iconsrc = `https://openweathermap.org/img/w/${weather.icon}`;
+    let desc = data.weather[0].description;
+    weatherIcon.setAttribute('src', iconsrc);
+    weatherIcon.setAttribute('___', _____);
+    captionDesc.textContent = `${desc}`;
+  }
+
+  displayResults();
